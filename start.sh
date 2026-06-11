537106276ee515772b5bfee38bdd92fe8969e105
Ali Deeb <alid68831@gmail.com>
2026-06-11 04:08:14 +0300

    fix port to 8080

--- start.sh
+++ start.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
 echo "Running migrations..."
-php artisan migrate --force
-
-echo "Linking storage..."
-php artisan storage:link || true
+php artisan migrate --force || echo "Migrate failed, continuing..."
 
 echo "PORT is: ${PORT}"
 echo "Starting server on port ${PORT:-8000}..."
-php artisan serve --host=0.0.0.0 --port=${PORT:-8000} 2>&1
+exec php artisan serve --host=0.0.0.0 --port=${PORT:-8080} 2>&1
