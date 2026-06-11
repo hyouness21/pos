2262c4a0e436aea0cf63ad96e663fa04b3212414
Ali Deeb <alid68831@gmail.com>
2026-06-11 22:53:29 +0300

    add railway init script and clean up start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,5 @@
 
 echo "PORT is: ${PORT}"
 
-echo "Running migrations..."
-php artisan migrate --force || echo "Migrate failed, continuing..."
-
 echo "Starting server on port ${PORT:-8080}..."
 exec php -S 0.0.0.0:${PORT:-8080} -t public 2>&1
